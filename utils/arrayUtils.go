425757efe4eec87e7cea176ccab85731f9effb52
Faruk Karadeniz <krdnzomer@gmail.com>
2020-07-03 16:55:12 +0300

    Add true random generator to mathUtils & Change data types to Int64

--- utils/arrayUtils.go
+++ utils/arrayUtils.go
@@ -5,12 +5,12 @@ import (
 )
 
 // CreateArray is a function that returns an array
-func CreateArray(size int) []int {
-	return make([]int, size)
+func CreateArray(size int) []int64 {
+	return make([]int64, size)
 }
 
 // SetValueAtIndex is a function that sets a value of an array at spesific index
-func SetValueAtIndex(arr *[]int, index int, value int) {
+func SetValueAtIndex(arr *[]int64, index int, value int64) {
 	if len(*arr) < index {
 		fmt.Println("Index shouldn't be bigger than array's length")
 	}
--- utils/mathUtils.go
+++ utils/mathUtils.go
@@ -1,10 +1,13 @@
 package utils
 
 import (
-	"math/rand"
+	"crypto/rand"
+	"math/big"
 )
 
 // GetRandomNumberBetween is a function that returns a random number between two numbers
-func GetRandomNumberBetween(start int, stop int) int {
-	return rand.Intn(stop-start) + start
+func GetRandomNumberBetween(start int64, stop int64) int64 {
+	n, _ := rand.Int(rand.Reader, big.NewInt(stop))
+	//return rand.Intn(stop-start) + start
+	return n.Int64() + start
 }
